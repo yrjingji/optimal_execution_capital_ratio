3122520125fec79c716f1d221270b8731328c7a9
yrjingji <ruijingyang094@gmail.com>
2023-01-30 16:12:20 -0500

    Update change_norm_portfolio_correlation.m

--- change_norm_portfolio_correlation.m
+++ change_norm_portfolio_correlation.m
@@ -2,7 +2,7 @@ sigma = [0.9 0.2;0.2 0.9];
 % we want to maintatin that the original variance of the portfolio is
 % constant with different correlations given
 X_0 = [305,305];
-k = 0.32;
+k = 0.3;
 cons_variance = X_0*sigma*X_0';
 rho_seq =  -0.5:0.05:0.9;
 parfor j = 1:length(rho_seq)
