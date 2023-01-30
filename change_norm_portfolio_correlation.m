18a17e6a3bc087908494cde2c541d17ddcc2ef9b
yrjingji <ruijingyang094@gmail.com>
2023-01-29 20:24:52 -0500

    Update change_norm_portfolio_correlation.m

--- change_norm_portfolio_correlation.m
+++ change_norm_portfolio_correlation.m
@@ -24,24 +24,28 @@ l = 1200;
 S_0 = [250,200];
 c1 = .1;
 c2 = 0.09;
-beta = c1*sigma;
-gamma = c2*sigma;
+for j = 1:length(rho_seq)
+    var = cons_variance/(X_0*[1 rho_seq(j); rho_seq(j) 1]*X_0');
+    sigma = var.*[1 rho_seq(j);rho_seq(j) 1];
+    beta = c1*sigma;
+    gamma = c2*sigma;
 %objective function
-A_obj = c1*sigma;
-B_obj = 0.5*c2*sigma;
-negative_P0 =[A_obj B_obj B_obj B_obj B_obj; B_obj A_obj B_obj B_obj B_obj;...
+    A_obj = c1*sigma;
+    B_obj = 0.5*c2*sigma;
+    negative_P0 =[A_obj B_obj B_obj B_obj B_obj; B_obj A_obj B_obj B_obj B_obj;...
     B_obj B_obj A_obj B_obj B_obj;B_obj B_obj B_obj A_obj B_obj;B_obj B_obj B_obj B_obj A_obj];
-negative_q0 = -[initial_price(1);initial_price(2);initial_price(1);initial_price(2);initial_price(1);initial_price(2);...
+    negative_q0 = -[initial_price(1);initial_price(2);initial_price(1);initial_price(2);initial_price(1);initial_price(2);...
     initial_price(1);initial_price(2);initial_price(1);initial_price(2)];
-s1 = ones(5,1)*S_0(1)/5;
-s2 = ones(5,1)*S_0(2)/5;
-naive_trading = [s1(1),s2(1),s1(2),s2(2),s1(3),s2(3),s1(4),s2(4),s1(5),s2(5)];
-value = -quad_form(naive_trading,negative_P0) - dot(negative_q0,naive_trading);
-plot(-0.5:0.1:0.5,opt_val_portfolio_rho,'b');
+    s1 = ones(5,1)*S_0(1)/5;
+    s2 = ones(5,1)*S_0(2)/5;
+    naive_trading = [s1(1),s2(1),s1(2),s2(2),s1(3),s2(3),s1(4),s2(4),s1(5),s2(5)];
+    value(j) = -quad_form(naive_trading,negative_P0) - dot(negative_q0,naive_trading);
+end
+plot(-0.5:0.1:0.8,opt_val_portfolio_rho,'b');
 hold on
-plot(-0.5:0.1:0.5,opt_special_portfolio_rho,'m');
-%yline(value,'r');
-legend('optimal trading strategy','affine trading strategy');%,'naive trading strategy');
+plot(-0.5:0.1:0.8,opt_special_portfolio_rho,'m');
+plot(-0.5:0.1:0.8,opt_special_portfolio_rho,'r')
+legend('optimal trading strategy','affine trading strategy','naive trading strategy');
 xlabel('correlation');
 ylabel('expected revenue');
 title('portfolio')
