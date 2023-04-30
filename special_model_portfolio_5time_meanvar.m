db47418530db170f29e7d339b745c7b186fc3a28
yrjingji <ruijingyang094@gmail.com>
2023-04-30 16:21:03 -0400

    Update special_model_portfolio_5time_meanvar.m

--- special_model_portfolio_5time_meanvar.m
+++ special_model_portfolio_5time_meanvar.m
@@ -66,13 +66,14 @@ function [opt_val,opt_mean,opt_var,sol_first,sol_second,prob_split_product] = sp
     
    %fourth step capital ratio constraint
     negative_A4 = [A,B,B,B;B,A,B,B;B,B,A,B;B,B,B,A];
-    negative_b4 = [ negative_b3,[(k(2)-1)*initial_price(2)+k(2)*X_0(2)*gamma(2,2) + k(1)*X_0(1)*gamma(2,1),....
-        (k(1)-1)*initial_price(1)+k(1)*X_0(1)*gamma(1,1)+k(2)*X_0(2)*gamma(1,2)]];
+    
+    negative_b4 = [ negative_b3,[(k(1)-1)*initial_price(1)+k(1)*X_0(1)*gamma(1,1)+k(2)*X_0(2)*gamma(1,2),....
+        (k(2)-1)*initial_price(2)+k(2)*X_0(2)*gamma(2,2) + k(1)*X_0(1)*gamma(2,1)]];
     negative_c4 = l - k(1)*X_0(1)*initial_price(1) - k(2)*X_0(2)*initial_price(2);
     %fourth step first chance constraint's coefficients
     D4(1,:) = sqrt(sigma(1,1)*tau)*[-k(1), 0,1-k(1),0,1-k(1),0,1-k(1),0];
     D4(2,:) = sqrt(sigma(2,2)*tau)*[0,-k(2), 0,1-k(2),0,1-k(2),0,1-k(2)];
-    D4(3,:) = sqrt(sigma(1,1)*tau)*[-k(1), 0,-k(1),0,1-k(1),0,0,1-k(1)];
+    D4(3,:) = sqrt(sigma(1,1)*tau)*[-k(1), 0,-k(1),0,1-k(1),0,1-k(1),0];
     D4(4,:) = sqrt(sigma(2,2)*tau)*[0,-k(2), 0,-k(2),0,1-k(2),0,1-k(2)];
     D4(5,:) = sqrt(sigma(1,1)*tau)*[-k(1), 0,-k(1),0,-k(1),0,1-k(1),0];
     D4(6,:) = sqrt(sigma(2,2)*tau)*[0,-k(2), 0,-k(2),0,-k(2),0,1-k(2)];
@@ -80,9 +81,6 @@ function [opt_val,opt_mean,opt_var,sol_first,sol_second,prob_split_product] = sp
     D4(8,:) = sqrt(sigma(2,2)*tau)*[0,-k(2), 0,-k(2),0,-k(2),0,-k(2)];
     
     e4 = [e3;[sqrt(sigma(1,1)*tau)*k(1)*X_0(1); sqrt(sigma(2,2)*tau)*k(2)*X_0(2)]];
-    
-    
-    
     %cholesky decompostion
     cov_1 = sigma;
     U1 = chol(cov_1);
